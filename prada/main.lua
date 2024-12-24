db9f6510062110558b3602882f86e849cf67b4e6
Efial <84471368+Efial@users.noreply.github.com>
2024-12-23 21:29:15 -0500

    Update main.lua

--- prada/main.lua
+++ prada/main.lua
@@ -75,4 +75,4 @@ getgenv().Prada = {
     }
 };
 --
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/edb36f00130ff5828031d590ba01b5c8.lua"))();
+script_key=getgenv().Prada['Key']; loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/edb36f00130ff5828031d590ba01b5c8.lua"))();
