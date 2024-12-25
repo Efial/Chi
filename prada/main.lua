90eb9a52b48c9021b3204b61d8f75aa2da551aa9
Efial <84471368+Efial@users.noreply.github.com>
2024-12-25 00:10:29 -0500

    Update main.lua

--- prada/main.lua
+++ prada/main.lua
@@ -1,1 +1,7 @@
+if not getgenv().executedprada1 then
+    getgenv().executedprada1 = true;
+else
+    return;
+end;
+--
 script_key=getgenv().Prada['Key']; loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/edb36f00130ff5828031d590ba01b5c8.lua"))();
